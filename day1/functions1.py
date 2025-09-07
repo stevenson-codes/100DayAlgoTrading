c304df2ec882ceea89842351bc487c18ba164262
Stevenson Yan <stevensoncodes@gmail.com>
2025-09-06 20:48:31 -0700

    day1

--- day1/functions1.py
+++ day1/functions1.py
@@ -1,21 +1,34 @@
-def calculate_sma(price, window):
+def calculate_sma(prices=[], window=1):
     """
     Calculate the Simple Moving Average (SMA) for the final window from the list of prices.
 
     Args:
-        price (list of float): List of historical prices.
+        prices (list of float): List of historical prices.
         window (int): The number of periods to use for calculating the moving average.
 
     Returns:
-        list of float: A list containing the SMA values. The length of the sreturned list will be len(price) - window + 1.
+        list of float: A list containing the SMA values. The length of the sreturned list will be len(prices) - window + 1.
 
     Raises:
-        ValueError: If window is less than 1 or greater than the length of price.
+        ValueError: If window is less than 1 or greater than the length of prices.
     """
 
-    if len(price) < window:
+    if len(prices) < window:
         print("ValueError: the length of the list of prices is less than the window size")
-    sma = sum(price[-window]) / window
+    return sum(prices[-window:]) / window
 
-prices = (10, 11, 12, 13, 14, 15, 16.5, 17.5)
-calculate_sma(prices, 5)
+prices = [10, 11, 12, 13, 14, 15, 16.5, 17.5]
+print(calculate_sma(prices, 5))
+print(calculate_sma.__doc__)
+
+def sum_prices(*prices):
+    return sum(prices)
+
+print(sum_prices(10, 20, 30))
+
+
+def desc_trade(**kwargs):
+    for key in kwargs:
+        print(f'{key} --> {kwargs[key]}')
+
+desc_trade(symbol='AAPL', result='profit')
